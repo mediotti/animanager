20489cea2fd6bd719e0b9b87d93605a780ab49a5
medzdev <medzdeveloper@gmail.com>
2022-10-13 21:32:46 -0300

    teste

---
+++ src/FichaCadastralTutor.java
@@ -0,0 +1,44 @@
+public class FichaCadastralTutor extends FichaCadastral implements FichaCadastralEndereco {
+    public FichaCadastralTutor() {
+    }
+
+    @Override
+    public String getEnderecoRua() {
+        return null;
+    }
+
+    @Override
+    public void setEnderecoRua(String enderecoRua) {
+
+    }
+
+    @Override
+    public String getEnderecoNum() {
+        return null;
+    }
+
+    @Override
+    public void setEnderecoNum(String enderecoNum) {
+
+    }
+
+    @Override
+    public String getEnderecoComplemento() {
+        return null;
+    }
+
+    @Override
+    public void setEnderecoComplemento(String enderecoComplemento) {
+
+    }
+
+    @Override
+    public String getEnderecoCEP() {
+        return null;
+    }
+
+    @Override
+    public void setEnderecoCEP(String enderecoCEP) {
+
+    }
+}
